8e36f971d186c3ee028a984b3d7587d2f2ccb030
ahatcher8 <drewhatcher@gmail.com>
2020-05-12 02:24:58 -0400

    Added the functionality to save playlists to a user's profile

--- src/components/PlaylistHeader/PlaylistHeader.js
+++ src/components/PlaylistHeader/PlaylistHeader.js
@@ -3,8 +3,10 @@ import Button from 'react-bootstrap/Button';
 
 import './PlaylistHeader.css';
 
+import {spotifyWebApi} from '../../pages/Home';
 import {globalPlaylist} from '../PlaylistGenerator/PlaylistGenerator';
 import {globalPlaylistName} from '../../pages/Home';
+import {globalPlaylistLength} from '../PlaylistLength/PlaylistLength';
 
 
 export default class PlaylistHeader extends Component {
@@ -15,6 +17,7 @@ export default class PlaylistHeader extends Component {
             playlistEmpty: false,
             buttonIsActive: false,
             buttonText: "Save",
+            userID: "",
         }
         if (globalPlaylist.length == 0) {
             this.setState({
@@ -26,7 +29,83 @@ export default class PlaylistHeader extends Component {
 
     onClick() {
         this.setState({ buttonIsActive: true });
-        this.setState({ buttonText : "Playlist Saved!"});
+        this.setState({ buttonText : "Playlist Saved!"}, this.getUserID);
+    }
+
+    getUserID() {
+        spotifyWebApi.getMe().then(
+            function(data) {
+                // console.log(data);
+                this.setState({ userID : data.id}, this.createPlaylist);
+            }.bind(this),
+            function (err) {
+                console.error(err);
+            }.bind(this)
+        );
+    }
+
+    createPlaylist() {
+        // console.log(this.state.userID)
+        spotifyWebApi.createPlaylist(this.state.userID, {
+            "name": globalPlaylistName,
+            "description": "Recommendation Playlist Made By Drew Hatcher's App",
+            "public": true
+        }).then(
+            function(data) {
+                // console.log(data);
+                this.setState({ newPlaylistData : data}, this.addSongsToPlaylist);
+            }.bind(this),
+            function (err) {
+                console.error(err);
+            }.bind(this)
+        );
+    }
+
+    addSongsToPlaylist() {
+        // console.log(globalPlaylist);
+        // console.log(this.state.newPlaylistData);
+        var trackURIs1 = [];
+        var trackURIs2 = [];
+        var i;
+        var j;
+        for (i = 0; i < Math.floor(globalPlaylistLength / 2); i++) {
+            trackURIs1.push(globalPlaylist[i].uri);
+        }
+        for (j = Math.floor(globalPlaylistLength / 2); j < globalPlaylist.length; j++) {
+            trackURIs2.push(globalPlaylist[j].uri);
+        }
+
+        // console.log(trackURIs1, trackURIs2);
+
+        spotifyWebApi.addTracksToPlaylist(this.state.userID, this.state.newPlaylistData.id, trackURIs1).then(
+            function(data) {
+                // console.log(data);
+                this.setState({ newPlaylistData : data}, this.addSongsToPlaylist);
+            }.bind(this),
+            function (err) {
+                console.error(err);
+            }.bind(this)
+        );
+
+        spotifyWebApi.addTracksToPlaylist(this.state.userID, this.state.newPlaylistData.id, trackURIs2).then(
+            function(data) {
+                // console.log(data);
+                this.setState({ newPlaylistData : data}, this.addSongsToPlaylist);
+            }.bind(this),
+            function (err) {
+                console.error(err);
+            }.bind(this)
+        );
+
+        // spotifyWebApi.addTracksToPlaylist(this.state.userID, this.state.newPlaylistData.id, undefined, undefined, {uris : trackURIs}).then(
+        //     function(data) {
+        //         console.log(data);
+        //         this.setState({ newPlaylistData : data}, this.addSongsToPlaylist);
+        //     }.bind(this),
+        //     function (err) {
+        //         console.error(err);
+        //     }.bind(this)
+        // );
     }
     
     render() {
